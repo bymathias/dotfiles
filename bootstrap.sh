2f2a3afa87064af12916819e5060dc03f16f98fa
Mathias Brouilly <bymathias@gmail.com>
2017-12-02 21:48:13 +0100

    clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,46 +7,31 @@
 set -euo pipefail
 
 
-dot_directory=".dotfiles"
-dot_directory_tmp=$(mktemp -dq ~/tmp/dotfiles.XXXXXX)
+directory_dot=".dotfiles"
+directory_tmp=$(mktemp -dq ~/tmp/dotfiles.XXXXXX)
+file_symlinks=(.bashrc .bash_profile .vim .vimrc .editorconfig .gitconfig .curlrc .wgetrc .tmux.conf)
 
-dot_git_repository='https://github.com/bymathias/dotfiles.git'
-dot_git_config=(user.name user.email github.user)
+echo $OSTYPE
 
-dot_file_symlinks=(.bashrc .bash_profile .vim .vimrc .inputrc .editorconfig .czrc)
-
-# Check if command exists
-__cmd_exists() {
-  command -v "$1" > /dev/null 2>&1
-}
-
-# Build `dot_file_symlinks` array if command exits
-if __cmd_exists "git"; then
-  dot_file_symlinks+=(.gitconfig)
-fi
-
-# [[ __cmd_exists "tmux" ]] && dot_file_symlinks+=(.tmux.conf)
-# [[ __cmd_exists "curl" ]] && dot_file_symlinks+=(.curlrc)
-# [[ __cmd_exists "wget" ]] && dot_file_symlinks+=(.wgetrc)
-# [[ __cmd_exists "wget" ]] && dot_file_symlinks+=(.conkyrc)
-
-echo ${dot_file_symlinks[@]}
+# ================================================
+#   Helper functions
+# ================================================
 
 # Remove symlinks and backup files/directories
-__file_remove() {
+_file_remove() {
   local file="$HOME/$1"
 
   if [ -h "$file" ]; then
     rm -v "$file"
   elif [ -f "$file" ] || [ -d "$file" ]; then
-    mv -v "$file" "$dot_dirtmp/$1"
+    mv -v "$file" "$directory_tmp/$1"
   fi
 }
 
 # Symlink dotfiles in $HOME
-__file_symlink() {
+_file_symlink() {
   local file="$HOME/$1"
-  local dir="$HOME/$dot_dir"
+  local dir="$HOME/$directory_dot"
 
   if [ "$1" == ".gitconfig" ]; then
     ln -sv "$dir/git/$1" "$file"
@@ -57,13 +42,13 @@ __file_symlink() {
   fi
 }
 
-# ========================================================
-#   Main functions
-# ========================================================
+# ================================================
+#   Main function
+# ================================================
 
-__bootstrap() {
+_bootstrap() {
   if [ $# -ne 1 ]; then
-    __bootstrap help
+    _bootstrap help
     return
   fi
   case "$1" in
@@ -71,21 +56,29 @@ __bootstrap() {
 
       echo "Install.."
 
-      for i in "${dot_symlinks[@]}"; do
-        __file_remove "$i"
-        __file_symlink "$i"
+      for i in "${file_symlinks[@]}"; do
+        _file_remove "$i"
+        _file_symlink "$i"
       done
+
+      # sudo ln -siv "$HOME/$directory_dot/.vimrc" "/root/.vimrc"
+      # sudo ln -siv "$HOME/$directory_dot/vim" "/root/.vim"
+
+      _bootstrap "update"
 
       ;;
     "uninstall")
 
       echo "Uninstall.."
 
-      for i in "${dot_symlinks[@]}"; do
-        __file_remove "$i"
+      for i in "${file_symlinks[@]}"; do
+        _file_remove "$i"
       done
 
-      __file_remove "$dot_dir"
+      # sudo rm -iv --preserve-root "/root/.vimrc"
+      # sudo rm -Riv --preserve-root "/root/.vim"
+
+      _file_remove "$directory_dot"
 
       ;;
     "update")
@@ -102,11 +95,11 @@ __bootstrap() {
     "help"|*)
       echo "Usage:"
       echo ""
-      echo "  ./bootstrap.sh [ install | uninstall | help ]"
+      echo "  ./bootstrap.sh [ install | uninstall | update | help ]"
       echo ""
       ;;
   esac
 }
 
 # run it
-time { __bootstrap "$@"; }
+time { _bootstrap "$@"; }
