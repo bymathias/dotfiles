f5fa83636e7bdcefe843bdc1b7aac15dd75f2620
Mathias Brouilly <bymathias@gmail.com>
2017-12-27 06:26:43 +0100

    removed dots

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,81 +7,125 @@
 # set -euo pipefail
 
 
-directory_dot=".dotfiles"
-directory_tmp=$(mktemp -dq ~/tmp/dotfiles.XXXXXX)
-file_symlinks=(.bashrc .bash_profile .vim .vimrc .editorconfig .gitconfig .curlrc .wgetrc .tmux.conf)
+tmp_directory=$(mktemp -dq ~/tmp/dotfiles.XXXXXX)
+ext_backup="$(date +'%Y-%m-%d').backup"
+
+dot_directory="$HOME/.dotfiles"
+file_symlinks=(bashrc bash_profile vim vimrc editorconfig gitconfig curlrc wgetrc tmux.conf)
+
+git_infos=(user.name user.email github.user)
+git_repository="https://github.com/bymathias/dotfiles.git"
 
 # ================================================
 #   Helper functions
 # ================================================
 
+# Print output underlined
+fn_print_info() {
+  printf "\n\e[0;4m$1\e[0m\n"
+}
+
+# Print output in green
+fn_print_success() {
+  printf "\e[0;32m[✔] $1\e[0m\n"
+}
+
+# Print output in red
+fn_print_error() {
+  printf "\e[0;31m[✖] $1\e[0m\n"
+}
+
+# Print output in yellow
+fn_print_question() {
+  printf "\e[0;33m[?] $1: \e[0m"
+}
+
+# Ask question, timeout after 8s with 'Yes' as default.
+fn_ask() {
+  fn_print_question "$1 (y/n)"
+  read -n 1 -t 8
+  REPLY=${REPLY:-y}
+  printf "\n"
+}
+
+# Return 0 if the answer is y/yes/Y/Yes
+fn_ask_is_yes() {
+	[[ "$REPLY" =~ ^[Yy]$ ]] && return 0 || return 1
+}
+
+# Check if command exists
+fn_cmd_exists() {
+  command -v "$1" > /dev/null 2>&1
+}
+
 # Remove symlinks and backup files/directories
-_file_remove() {
-  local file="$HOME/$1"
-
-  if [ -h "$file" ]; then
-    rm -v "$file"
-  elif [ -f "$file" ] || [ -d "$file" ]; then
-    mv -v "$file" "$directory_tmp/$1"
+fn_file_remove() {
+  if [ -h "$1" ]; then
+    rm -v "$1"
+  elif [ -f "$1" ] || [ -d "$1" ]; then
+    mv -v "$1" "$1.$ext_backup"
   fi
 }
 
-# Symlink dotfiles in $HOME
-_file_symlink() {
-  local file="$HOME/$1"
-  local dir="$HOME/$directory_dot"
+fn_file_symlink() {
+  local link="$2$1"
 
-  if [ "$1" == ".gitconfig" ]; then
-    ln -sv "$dir/git/$1" "$file"
-  elif [ "$1" == ".vim" ]; then
-    ln -sv "$dir/vim" "$file"
-  else
-    ln -sv "$dir/$1" "$file"
+  fn_file_remove "$link"
+  ln -sv "$dot_directory/$1" "$link"
+}
+
+fn_edit_gitconfig() {
+  local info
+  info=$(git config --global --get "$1" || echo "")
+
+  if [ -z "$info" ]; then
+    fn_print_question "Enter your git \"$1\""
+    read -r -t 10 ans
+    info=$ans
   fi
+
+  git config --file "$dot_directory/gitconfig" --replace-all "$1" "$info"
 }
 
 # ================================================
-#   Main function
+#   Main `bootstrap.sh` function
 # ================================================
 
-_bootstrap() {
+fn_bootstrap() {
   if [ $# -ne 1 ]; then
-    _bootstrap help
+    fn_bootstrap help
     return
   fi
   case "$1" in
+
     "install")
 
-      echo "Install.."
-
-      for i in "${file_symlinks[@]}"; do
-        _file_remove "$i"
-        _file_symlink "$i"
+      fn_print_info "Edit \"git\" infos user.."
+      for i in "${git_infos[@]}"; do
+        fn_edit_gitconfig "$i"
       done
 
-      # sudo ln -siv "$HOME/$directory_dot/.vimrc" "/root/.vimrc"
-      # sudo ln -siv "$HOME/$directory_dot/vim" "/root/.vim"
+      fn_print_info "Symlink .dotfiles to home.."
+      for i in "${file_symlinks[@]}"; do
+        fn_file_symlink "$i" "$HOME/."
+      done
 
-      _bootstrap "update"
+      fn_print_info "Install vim plugins.."
+      vim +PlugInstall +qall
 
       ;;
+
     "uninstall")
 
-      echo "Uninstall.."
-
+      fn_print_info "Remove symlinks and move .dotfiles folder.."
       for i in "${file_symlinks[@]}"; do
-        _file_remove "$i"
+        fn_file_remove "$i"
       done
-
-      # sudo rm -iv --preserve-root "/root/.vimrc"
-      # sudo rm -Riv --preserve-root "/root/.vim"
-
-      _file_remove "$directory_dot"
+      fn_file_remove "$dot_directory"
 
       ;;
-    "update")
 
-      echo "Update.."
+    "update")
 
       # Vim plugins with vim-plug
       # - Remove unused directories (bang version will clean without prompt)
@@ -90,19 +134,16 @@ _bootstrap() {
       vim +PlugClean! +PlugUpgrade +PlugUpdate +qall
 
       ;;
-    "test")
 
-      echo "Test.."
-
-      ;;
     "help"|*)
+
       echo "Usage:"
       echo ""
       echo "  ./bootstrap.sh [ install | uninstall | update | help ]"
       echo ""
+
       ;;
   esac
 }
 
-# run it
-time { _bootstrap "$@"; }
+time { fn_bootstrap "$@"; }
