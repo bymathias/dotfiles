6ca13f22cdb694244e0770a8ec7ead20955a0cc8
Mathias Brouilly <bymathias@gmail.com>
2020-10-19 08:26:52 +0200

    Add Snap path

--- bash/paths.bash
+++ bash/paths.bash
@@ -5,5 +5,8 @@ export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:$PATH"
 # (first) Add RVM to PATH for scripting
 [[ -d "$HOME/.rvm/bin" ]] && export PATH="$PATH:$HOME/.rvm/bin"
 
+# Add Snap to PATH
+[[ -d "/snap/bin" ]] && export PATH="$PATH:/snap/bin"
+
 # Local bin scripts
 [[ -d "$HOME/.dotfiles/bin" ]] && export PATH="$PATH:$HOME/.dotfiles/bin"
